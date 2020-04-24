cb838cb861575d695b596dc847121da4caef5deb
Franchell Polanco <polanco.franchell@gmail.com>
2020-04-24 13:33:52 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def find_min_value(array)
   count = 0
-  min_value = 0
+  min_value = -1
   while count < array.length do
   if min_value > array[count]
     min_value = array[count]
